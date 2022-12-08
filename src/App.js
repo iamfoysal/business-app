1e6948a92fa48ac41d638635a2b960c4b48732e0
iamfoysal <foysalf652@gmail.com>
2022-12-09 01:11:49 +0600

    Module 35 initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ExpenseItem from './components/ExpenseItem';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      {/* <h2>Hello React </h2>
+      <p>This is also visible </p> */}
+       <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App
+
+
