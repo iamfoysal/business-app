aa9df2d57d105c9af694a4d0564d1589067c2f2f
iamfoysal <foysalf652@gmail.com>
2022-12-24 01:20:57 +0600

    55. Updating State That Depends On The Previous State

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,62 +3,74 @@ import "./ExpenseForm.css";
 import { useState } from "react";
 
 const ExpenseForm = () => {
-    // const [enterdTitle, setEnterdTitle] = useState('');
-    // const [enterdAmount, setEnterdAmount] = useState('');
-    // const [enterdDate, setEnterdDate] = useState('');
+  const [enterdTitle, setEnterdTitle] = useState("");
+  const [enterdAmount, setEnterdAmount] = useState("");
+  const [enterdDate, setEnterdDate] = useState("");
 
-    const [userInput, setUserInput] = useState({
-        enterdTitle: '',
-        enterdAmount: '',
-        enterdDate: '',
-    });
+  // const [userInput, setUserInput] = useState({
+  //   enterdTitle: "",
+  //   enterdAmount: "",
+  //   enterdDate: "",
+  // });
 
+  const titleChangehandler = (event) => {
+    setEnterdTitle.log(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enterdTitle: event.target.value,
+    // });
 
-    const titleChangehandler = (event) =>{
-        // setEnterdTitle.log(event.target.value)
-        setUserInput({
-            ...userInput,
-            enterdTitle: event.target.value
-        });
-    }
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enterdTitle: event.target.value };
+    // });
+  };
 
-    const amountChangehandler = (event) =>{
-        // setEnterdAmount.log(event.target.value)
-        setUserInput({
-            ...userInput,
-            enterdAmount: event.target.value
-        });
+  const amountChangehandler = (event) => {
+    setEnterdAmount.log(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enterdAmount: event.target.value,
+    // });
+  };
 
-    }
+  const dateChangehandler = (event) => {
+    setEnterdDate.log(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enterdDate: event.target.value,
+    // });
+  };
 
-    const dateChangehandler = (event) =>{
-        // setEnterdDate.log(event.target.value)
-        setUserInput({
-            ...userInput,
-            enterdDate: event.target.value
-        });
-    }
-
-  return(
+  return (
     <form>
-    <div className='new-expense__controls'>
-      <div className='new-expense__control'>
-        <label>Title</label>
-        <input type='text' onChange={titleChangehandler} />
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input type='text' onChange={titleChangehandler} />
+        </div>
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            onChange={amountChangehandler}
+          />
+        </div>
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input
+            type='date'
+            min='2000-01-01'
+            max='2025-12-31'
+            onChange={dateChangehandler}
+          />
+        </div>
       </div>
-      <div className='new-expense__control'>
-        <label>Amount</label>
-        <input type='number' min='0.01' step='0.01' onChange={amountChangehandler} />
+      <div className='new-expense__actions'>
+        <button type='submit'>Add Expense</button>
       </div>
-      <div className='new-expense__control'>
-        <label>Date</label>
-        <input type='date' min='2000-01-01' max='2025-12-31' onChange={dateChangehandler} />
-      </div>
-    </div>
-    <div className='new-expense__actions'>
-      <button type='submit'>Add Expense</button>
-    </div>
-  </form>
+    </form>
   );
 };
 
